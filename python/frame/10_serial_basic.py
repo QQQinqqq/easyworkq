647a578e07053054a85978f91d4eeccfb4074045
QQQinqqq <66257854+QQQinqqq@users.noreply.github.com>
2021-04-13 11:48:33 +0800

    Update 10_serial_basic.py

--- python/frame/10_serial_basic.py
+++ python/frame/10_serial_basic.py
@@ -1,13 +1,21 @@
 import serial
+import time
 
 def ReceiveData(serial):
     tmp_wait = serial.inWaiting()
-    if tmp_wait !=0：
-        serial.read(tmp_wait)
-    else:
+    if tmp_wait == 0:
         print('no data')
+        return ''
+    data = serial.read(tmp_wait)
+    data_hex = data.hex()
+    data_str = ''
+    for i in range(len(data_hex)):
+        data_str += data_hex[i]
+        if i%2 == 1:
+            data_str += ' '
+    return data_str.strip().upper()
 
-ser = serial.Serial('com1'，9600，timeout=1)
+ser = serial.Serial('com1', 9600, timeout=1)
 frame = '68 0F 00 43 00 00 00 00 00 00 10 40 0D A0 16'
 frame_list = frame.strip().split(' ')
 for i in range(len(frame_list)):
@@ -16,6 +24,7 @@ for i in range(len(frame_list)):
 send_data = bytes(frame_list)
 ser.write(send_data)
 time.sleep(0.5)
-receive_data = ReceiceData(ser)
+receive_data = ReceiveData(ser)
+print(receive_data)
 
 ser.close()
